f9d431782681545fb796f901e712e49509207560
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 22:57:14 +0300

    Add packagingRoutes placeholder

---
+++ routes/packaging.js
@@ -0,0 +1,11 @@
+const packagingRoutes = async (fastify) => {
+    fastify.post('/packaging/pack', async (request, reply) => {
+
+    });
+
+    fastify.post('/packaging/unpack', async (request, reply) => {
+
+    });
+};
+
+export default packagingRoutes;
