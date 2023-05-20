55e76e7765c57448183cf021db7a9e210762a338
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 22:57:49 +0300

    Add constants for service paths

---
+++ paths/index.js
@@ -0,0 +1,7 @@
+const IDENTITY_SERVICE_PATH = 'identity-service.herokuapp.com';
+
+const FILE_SERVICE_PATH = 'file-service.herokuapp.com';
+
+const FILE_LINK_SERVICE_PATH = 'file-link-service.herokuapp.com';
+
+const ARCHIVE_SERVICE_PATH = 'archive-service.herokuapp.com';
