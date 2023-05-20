18f3f8711b68fb3ebd09462c5bf28d2d7ce6e61d
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 23:33:56 +0300

    Fill '/user/me' placeholder

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
+import axios from 'axios';
+
 import { createPath } from "../paths";
 
 const userRoutes = async (fastify) => {
     const getFullPath = createPath('IDENTITY');
 
     fastify.get('/user/me', async (request, reply) => {
+        const { query, headers, body } = request;
 
+        try {
+            const response = await axios.get(getFullPath('/me'), {
+                params: query,
+                headers: headers,
+                data: body
+            });
+
+            reply.send(response.data);
+        } catch (error) {
+            reply.code(500).send('Error occurred while making the request.');
+        }
     });
 
     fastify.post('/user/register', async (request, reply) => {
