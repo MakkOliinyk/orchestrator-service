6787d60542e6392f05a58e36772d948440433a5c
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-20 23:31:43 +0300

    Expand paths config with createPath

--- paths/index.js
+++ paths/index.js
@@ -1,7 +1,19 @@
 const IDENTITY_SERVICE_PATH = 'identity-service.herokuapp.com';
-
 const FILE_SERVICE_PATH = 'file-service.herokuapp.com';
-
 const FILE_LINK_SERVICE_PATH = 'file-link-service.herokuapp.com';
-
 const ARCHIVE_SERVICE_PATH = 'archive-service.herokuapp.com';
+
+const PATHS_BY_SERVICE = {
+    'IDENTITY': IDENTITY_SERVICE_PATH,
+    'FILE': FILE_SERVICE_PATH,
+    'FILE_LINK': FILE_LINK_SERVICE_PATH,
+    'PACKAGING': ARCHIVE_SERVICE_PATH
+};
+
+export const createPath = (serviceType) => (endRoute) => {
+    if (!serviceType) throw new Error('Service type is required');
+    if (!endRoute) throw new Error('End route is required');
+    if (!PATHS_BY_SERVICE[serviceType]) throw new Error('Invalid service type');
+
+    return PATHS_BY_SERVICE[serviceType] ? `https://${PATHS_BY_SERVICE[serviceType]}/${endRoute}` : '';
+};
