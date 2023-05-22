670ea30f836a3d83ea25e0efb0d0befe5b9374b0
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-22 23:10:04 +0300

    Add FileLink and Packaging paths

--- paths/index.js
+++ paths/index.js
@@ -1,13 +1,13 @@
 const IDENTITY_SERVICE_PATH = 'identity-service.herokuapp.com';
 const FILE_SERVICE_PATH = 'file-service.herokuapp.com';
-const FILE_LINK_SERVICE_PATH = 'file-link-service.herokuapp.com';
-const ARCHIVE_SERVICE_PATH = 'archive-service.herokuapp.com';
+const FILE_LINK_SERVICE_PATH = 'us-central1-file-links.cloudfunctions.net/app';
+const PACKAGING_SERVICE_PATH = 'us-central1-packaging-service.cloudfunctions.net/app';
 
 const PATHS_BY_SERVICE = {
     'IDENTITY': IDENTITY_SERVICE_PATH,
     'FILE': FILE_SERVICE_PATH,
     'FILE_LINK': FILE_LINK_SERVICE_PATH,
-    'PACKAGING': ARCHIVE_SERVICE_PATH
+    'PACKAGING': PACKAGING_SERVICE_PATH
 };
 
 export const createPath = (serviceType) => (endRoute) => {
@@ -15,5 +15,5 @@ export const createPath = (serviceType) => (endRoute) => {
     if (!endRoute) throw new Error('End route is required');
     if (!PATHS_BY_SERVICE[serviceType]) throw new Error('Invalid service type');
 
-    return PATHS_BY_SERVICE[serviceType] ? `https://${PATHS_BY_SERVICE[serviceType]}/${endRoute}` : '';
+    return PATHS_BY_SERVICE[serviceType] ? `http://${PATHS_BY_SERVICE[serviceType]}/${endRoute}` : '';
 };
