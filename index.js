9d096d2249c80715dd8d306957f2ce28314f829f
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-23 22:28:03 +0300

    Apply POST & PUT fix to fastify

--- index.js
+++ index.js
@@ -5,20 +5,28 @@ import fileLinkServiceRoutes from './routes/file-link';
 import packagingServiceRoutes from './routes/packaging';
 import userServiceRoutes from './routes/user';
 
-const app = fastify({ logger: true });
+let requestHandler = null;
+
+const app = fastify({
+    logger: true,
+    serverFactory: (handler) => {
+        requestHandler = handler;
+        return require('http').createServer();
+    },
+});
+
+app.addContentTypeParser('application/json', {}, (req, body, done) => {
+    done(null, body.body);
+});
 
 app.register(userServiceRoutes);
 app.register(fileServiceRoutes);
 app.register(fileLinkServiceRoutes);
 app.register(packagingServiceRoutes);
 
-const start = async () => {
-    try {
-        await app.listen(process.env.PORT || 5000, '0.0.0.0');
-    } catch (err) {
-        app.log.error(err);
-        process.exit(1);
-    }
-};
-
-start();
+exports.app = functions.https.onRequest((req, res) => {
+    app.ready((err) => {
+        if (err) throw err;
+        requestHandler(req, res);
+    });
+});
