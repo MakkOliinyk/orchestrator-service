56ca1b31f2fbdb3c68413df2e17bae0279fc8d87
Mark Oliinyk <markoliinyk@gmail.com>
2023-05-25 22:43:45 +0300

    Init paths util

--- functions/paths/index.js
+++ functions/paths/index.js
@@ -1,7 +1,7 @@
-const IDENTITY_SERVICE_PATH = 'us-central1-identity-service-1fa8e.cloudfunctions.net/app ';
+const IDENTITY_SERVICE_PATH = 'us-central1-identity-service-1fa8e.cloudfunctions.net/app';
 const FILE_SERVICE_PATH = 'us-central1-file-service-87ddd.cloudfunctions.net/app';
 const FILE_LINK_SERVICE_PATH = 'us-central1-file-links.cloudfunctions.net/app';
-const PACKAGING_SERVICE_PATH = 'us-central1-packaging-service.cloudfunctions.net/app';
+const PACKAGING_SERVICE_PATH = 'us-central1-packaging-service.cloudfunctions.net/myApp';
 
 const PATHS_BY_SERVICE = {
     'IDENTITY': IDENTITY_SERVICE_PATH,
@@ -10,10 +10,10 @@ const PATHS_BY_SERVICE = {
     'PACKAGING': PACKAGING_SERVICE_PATH
 };
 
-export const createPath = (serviceType) => (endRoute) => {
+module.exports = createPath = (serviceType) => (endRoute) => {
     if (!serviceType) throw new Error('Service type is required');
     if (!endRoute) throw new Error('End route is required');
     if (!PATHS_BY_SERVICE[serviceType]) throw new Error('Invalid service type');
 
-    return PATHS_BY_SERVICE[serviceType] ? `http://${PATHS_BY_SERVICE[serviceType]}/${endRoute}` : '';
+    return PATHS_BY_SERVICE[serviceType] ? `https://${PATHS_BY_SERVICE[serviceType]}${endRoute}` : '';
 };
